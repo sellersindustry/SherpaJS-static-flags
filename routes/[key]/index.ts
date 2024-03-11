9f7bd88a20748ca1698784fbc313e7077df21e91
Evan Sellers <github@sellersew.com>
2024-03-11 10:10:53 -0400

    Added Footers + Headers

--- routes/[key]/index.ts
+++ routes/[key]/index.ts
@@ -1,3 +1,16 @@
+/*
+ *   Copyright (C) 2024 Sellers Industries, Inc.
+ *   distributed under the MIT License
+ *
+ *   author: Evan Sellers <sellersew@gmail.com>
+ *   date: Mon Mar 11 2024
+ *   file: index.ts
+ *   project: SherpaJS Static Flags
+ *   purpose: Flag by Key
+ *
+ */
+
+
 import { Response, Request, Environment } from "sherpa-core";
 
 
@@ -10,3 +23,8 @@ export function GET(req:Request, env:Environment) {
         return Response("", { status: 404 });
     }
 }
+
+
+// Teach me your way, Lord, that I may rely on your faithfulness; give me an
+// undivided heart, that I may fear your name.
+// - Psalm 86:11
--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,23 @@
+/*
+ *   Copyright (C) 2024 Sellers Industries, Inc.
+ *   distributed under the MIT License
+ *
+ *   author: Evan Sellers <sellersew@gmail.com>
+ *   date: Mon Mar 11 2024
+ *   file: index.ts
+ *   project: SherpaJS Static Flags
+ *   purpose: All Flags
+ *
+ */
+
+
 import { Response, Request, Environment } from "sherpa-core";
 
 
 export function GET(_:Request, env:Environment) {
     return Response(env.GetProperties());
 }
+
+
+// The Lord will rescue his servants; no one who takes refuge in him will be condemned.
+// - Psalm 34:22
